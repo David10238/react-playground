ecd8fb0cda72769f7e30bc77fbed74d7051f88d7
David Lukens <davidqcy02@gmail.com>
2021-03-19 20:26:41 -0500

    Add properties to class and function components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 
-const FunctionComponent = () => (
+const FunctionComponent = (props) => (
   <div>
+    <p>Hello from the function {props.name}</p>
     <p>this is a component from a function</p>
     <p>this is part of the same function component</p>
   </div>
 );
+
+const drinkingAge = (age) => {
+  if (age < 21) return "is two young to drink";
+  return "is old enough to drink";
+};
 
 class ClassComponent extends Component {
   render() {
     return (
       <div>
         <p>This is part of a component from a class.</p>
+        <p>Hello from the class {this.props.name}</p>
+        <p>
+          {this.props.name} {drinkingAge(this.props.age)}
+        </p>
         <p>This is part of the same class component</p>
       </div>
     );
@@ -38,8 +48,9 @@ function App() {
         </a>
       </header>
       <body>
-        <ClassComponent />
-        <FunctionComponent />
+        <ClassComponent name="Freddy" age={12} />{" "}
+        {/* non strings need to be sorrunded by */}
+        <FunctionComponent name="John" />
       </body>
     </div>
   );
