059b3b6d99c7531605e9120f81639dc4e0dd7cbf
David Lukens <davidqcy02@gmail.com>
2021-03-28 20:43:12 -0500

    lifting state

--- src/App.js
+++ src/App.js
@@ -67,9 +67,36 @@ const ReactHeader = () => (
 class App extends Component {
   state = {
     visible: true,
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
   };
 
   render() {
+    return (
+      <div>
+        <Counter
+          count={this.state.count}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+        <Counter
+          count={this.state.count}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+      </div>
+    );
     return <FetchingAPIData />;
     return <ValidatedForm />;
     // not returning this because I'm doing other stuff
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,38 +1,12 @@
 import React from "react";
 
 export default class Counter extends React.Component {
-  // need to be class to use state
-  constructor(props) {
-    super(props);
-
-    // doesn't have to be withen a constructor
-    this.state = {
-      count: 0,
-    };
-  }
-
-  handleButtonClick1 = () => {
-    // shorcut to bind the function
-    console.log("Button pressed");
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  handleButtonClick2 = () => {
-    // shorcut to bind the function
-    console.log("Button pressed");
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
-
   render() {
     return (
       <div>
-        <p>count: {this.state.count}</p>
-        <button onClick={this.handleButtonClick1}>increment</button>
-        <button onClick={this.handleButtonClick2}>decrement</button>
+        <div>count: {this.props.count}</div>
+        <button onClick={this.props.increment}>increment</button>
+        <button onClick={this.props.decrement}>decrement</button>
       </div>
     );
   }
