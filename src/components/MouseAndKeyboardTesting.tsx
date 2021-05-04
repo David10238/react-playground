75e2d3bd3572641072a3c2b7bb1d993267ca31a4
David Lukens <davidqcy02@gmail.com>
2021-05-03 22:41:14 -0500

    add "live" checking for space bar

--- src/components/MouseAndKeyboardTesting.tsx
+++ src/components/MouseAndKeyboardTesting.tsx
@@ -1,33 +1,64 @@
 import { useEffect, useState } from "react";
 
 
-const MouseAndKeyboardTesting = ()=> {
+const KeyboardTest = ()=> {
 
     const [timesSaved, setTimesSaved] = useState(0);
 
+    // replace with the dictionary
+    const [spaceDown, setSpaceDown] = useState(false);
+
     const handleKeyDown = (event:KeyboardEvent) => {
+        console.log("event with key ", event.key)
         if(event.key == 's' && event.ctrlKey){
             event.preventDefault();
             console.log("A key was pressed", event.key);
             setTimesSaved(timesSaved + 1);
         }
+
+        if(event.key == ' ')
+            setSpaceDown(true);
+    }
+
+    const handleKeyUp = (event:KeyboardEvent) => {
+        if(event.key == ' ')
+            setSpaceDown(false);
     }
 
     // only needs to be run when it's rendered, empty [] prevents it from being recreated each component change
     useEffect(() => {
+        // keypress is character only. keydown is allkeys
         window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keyup', handleKeyUp);
 
         // cleanup the component
         return () =>{
-            window.removeEventListener('keydown', handleKeyDown)
+            window.removeEventListener('keydown', handleKeyDown);
+            window.removeEventListener('keyup', handleKeyUp);
         }
       });// also works with [timesSaved]
 
     return (
-        <div>
+        <div> 
             <p>Times saved {timesSaved}</p>
+            <p>{spaceDown ? "True" : "False"}</p>
         </div>
     );
-}
+};
+
+const MouseTest = () =>{
+    return(
+        <div>
+        </div>
+    )
+};
+
+const MouseAndKeyboardTesting = () =>(
+    <div>
+        <KeyboardTest/>
+        <p/>
+        <MouseTest/>
+    </div>
+);
 
 export default MouseAndKeyboardTesting
